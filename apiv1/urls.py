ff39647adcb78497ff8a88a96e51c095160b564e
Mr_win7 <wwq380820271@163.com>
2018-05-12 02:14:15 +0800

    finish v1

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from apiv1.views import *
 
 routers = DefaultRouter()
-routers.register(r'document/search', DocumentViewSet， base_name='document_search')
+routers.register(r'document/search', DocumentViewSet, base_name='document_search')
 
 urlpatterns = [
     url(r'^', include(routers.urls)),
