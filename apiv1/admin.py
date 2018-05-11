62218e3d10df195d405dd152e68f9ec538fe63fb
Mr_win7 <wwq380820271@163.com>
2018-05-12 02:20:25 +0800

    finish v1

--- apiv1/admin.py
+++ apiv1/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'department', 'url', 'time')
