e8be63b3be6003f4e9dcdc127d17d9f7ed21d85b
Mr_win7 <wwq380820271@163.com>
2018-08-18 19:10:48 +0800

    add pagination

--- MatchSearchEngine/settings.py
+++ MatchSearchEngine/settings.py
@@ -131,11 +131,11 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-'''
 REST_FRAMEWORK = {
-        'DEFAULT_FILTER_BACKENDS':('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS':('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
-'''
 
 HAYSTACK_CONNECTIONS = {
     'default': {
