69b4409d75fa6eba6b561cfc454db274cc2ce7ad
Mr_win7 <wwq380820271@163.com>
2018-08-18 20:52:59 +0800

    add pagination size query param

--- apiv1/views.py
+++ apiv1/views.py
@@ -7,13 +7,16 @@ from rest_framework import viewsets, filters
 from apiv1.models import *
 from apiv1.serializers import *
 from drf_haystack.viewsets import HaystackViewSet
+
 from apiv1.filters import SimplifiedHaystackFilter
+from apiv1.pagination import DocumentPageNumberPagination
 
 # Create your views here.
 
 class DocumentViewSet(HaystackViewSet):
     index_models = [Document]
     serializer_class = MainDocumentSearchSerializer
+    pagination_class = DocumentPageNumberPagination
     filter_backends = [SimplifiedHaystackFilter, filters.OrderingFilter, ]
     ordering_fields = ['time',]
 
